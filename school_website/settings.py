d66205b7d47a0fb5197e3dac4ad26c4b9af22265
Bekzo <forgithub243@gmail.com.com>
2025-07-17 16:55:36 +0500

    url fix

--- school_website/settings.py
+++ school_website/settings.py
@@ -14,8 +14,8 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'jazzmin',  
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
